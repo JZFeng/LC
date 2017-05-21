f8493d33c12604d396ffaf491382779910cd1191
JZFeng <matthew.fjz@gmail.com>
2017-05-21 16:42:17 -0700

    x

--- src/jz/HelloWorld.java
+++ src/jz/HelloWorld.java
@@ -11,6 +11,11 @@ import jz.DoubleLinkedList.ListNode;
 public class HelloWorld {
 
 	public static void main(String[] args) {
+	    
+	    String s = "   JZ  Feng    20   ";
+	    String[] strs = s.trim().split("\\s{1,} ");
+	    for(String str: strs)
+	        System.out.println(str);
 	}
 	
 
