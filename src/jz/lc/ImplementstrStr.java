3020206a38f838b38ee0ff28317df76b3807801d
JZFeng <matthew.fjz@gmail.com>
2017-05-29 10:05:22 -0700

    Use two pointers. Every time when it does not match, reset j = 0; it means there are j-1 characters matched. reset i = i -[j-1];

--- src/jz/lc/ImplementstrStr.java
+++ src/jz/lc/ImplementstrStr.java
@@ -22,11 +22,39 @@ public class ImplementstrStr
 
     public static void main(String[] args)
     {
+        
+        String haystack = "abababababcdabcdef";
+        String needle = "abc";
+        System.out.println(haystack.indexOf(needle));
+        System.out.println(strStr(haystack, needle));
 
     }
     
-    public int strStr(String haystack, String needle) {
-        return 0;
+    //Use two pointers. Every time when it does not match, reset j = 0; i = i - (j-1); 
+    public static int strStr(String haystack, String needle) {
+        
+        int i = 0 ; int j = 0;
+        
+        while(i < haystack.length() && j < needle.length())
+        {
+            if(haystack.charAt(i) == needle.charAt(j))
+            {
+                i++;
+                j++;
+            }
+            else
+            {
+                if(i == haystack.length()) return -1;
+                i = i-j+1;
+                j = 0;
+            }
+                
+        }
+        
+        if(j == needle.length())
+            return i-j;
+        else
+             return -1;
         
     }
 
