9782abff10f87705b136099047416944fedc972f
JZFeng <matthew.fjz@gmail.com>
2017-05-30 14:19:50 -0700

    practice two functions: * remove excessive white space; * countAndSay helper(String s);

--- src/jz/HelloWorld.java
+++ src/jz/HelloWorld.java
@@ -11,12 +11,55 @@ import jz.DoubleLinkedList.ListNode;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-	    String s = "   2    5    6 7     4       8      9     3     1    0   ";
-	    String[] nums = s.trim().split("\\s{1,}");
-	    for(String num: nums)
-	        System.out.print(Integer.parseInt(num)+" ");
+	    String s = "312211";
+	    System.out.println(helper(s));
+	 
 	}
 	
+	//"1","11","111221", "312211"
+    private static String helper(String s)
+    {
+        int count = 0;
+        StringBuilder sb = new StringBuilder();
+        int i = 0 ;
+        for(; i < s.length(); i++)
+        {
+            if(i == 0) count++;
+            else if(s.charAt(i) == s.charAt(i-1)) count++;
+            else
+            {
+                sb.append(count);
+                sb.append(s.charAt(i-1));
+                count = 1;
+            }
+        }
+        
+        sb.append(count);
+        sb.append(s.charAt(i-1));
+        
+        return sb.toString();
+        
+    }
+    
+	
+	public static String removeExcessWhileSpace(String s)
+	{
+	    if(s == null || s.length() ==0)
+	        return null;
+	    s = s.trim();
+	    //s = "   2    5    6 7     4       8      9     3     1    0   ";
+	    StringBuilder sb = new StringBuilder();
+	    int i = 0;
+	    for(;i < s.length()-1; i++)
+	    {
+	        if(s.charAt(i) ==  ' ' && s.charAt(i+1) == ' ')
+	            continue;
+	        else
+	            sb.append(s.charAt(i));
+	    }
+	    sb.append(s.charAt(i));
+	    return sb.toString().trim();
+	}
 
-	 
+	
 }
