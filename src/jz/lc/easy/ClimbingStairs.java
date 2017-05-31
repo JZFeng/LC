40132d110dbb5621e4ac45b820c4808a2b6d0bb0
JZFeng <matthew.fjz@gmail.com>
2017-05-31 16:37:05 -0700

    x

--- src/jz/lc/easy/ClimbingStairs.java
+++ src/jz/lc/easy/ClimbingStairs.java
@@ -10,7 +10,7 @@ Each time you can either climb 1 or 2 steps. In how many distinct ways can you c
 Note: Given n will be a positive integer.
 
  */
-package jz.lc;
+package jz.lc.easy;
 
 /**
  *@author jzhfeng
@@ -21,13 +21,38 @@ public class ClimbingStairs
 
     public static void main(String[] args)
     {
-
+        long startTime = System.currentTimeMillis();
+        System.out.println(climbStairs(100));
+        System.out.println("non-recursion running time:  " + (System.currentTimeMillis() - startTime));
+        
+        startTime = System.currentTimeMillis();
+        System.out.println(climbStairsII(100));
+        System.out.println("Recursion running time:  " + (System.currentTimeMillis() - startTime));
     }
 
-    public int climbStairs(int n)
+    public static long climbStairs(int n)
     {
-        return n;
-
+        if(n < 0 ) return 0;
+        if(n <=2) return n;
+        
+        long[] ways = new long[n];
+        ways[0] = 1; ways[1] =2;
+        for(int i = 2; i < n ; i++)
+        {
+            ways[i] = ways[i-1] + ways[i-2];
+        }
+        
+        return ways[n-1]; 
     }
 
+    public static long climbStairsII(int n)
+    {
+        if (n < 0)
+            return 0;
+        if (n <= 2)
+            return n;
+
+        return climbStairsII(n - 1) + climbStairsII(n - 2);
+
+    }
 }
