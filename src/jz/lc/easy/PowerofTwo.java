c5a24f606b40711d6d380bf75b30e30ff127613c
JZFeng <matthew.fjz@gmail.com>
2017-05-31 21:40:47 -0700

    bit manipulation

---
+++ src/jz/lc/easy/PowerofTwo.java
@@ -0,0 +1,35 @@
+/*
+ https://leetcode.com/problems/power-of-two/#/description
+ 
+ 231. Power of Two
+ 
+ Given an integer, write a function to determine if it is a power of two.
+ 
+ */
+package jz.lc.easy;
+
+public class PowerofTwo
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println(isPowerOfTwo(9));
+
+    }
+    
+    public static boolean isPowerOfTwo(int n) {
+        if(n <1) return false;
+
+        int count = 0;
+        
+        for(int i = 0 ; i < 32;i++)
+        {
+            if((n & 1) == 1) count++;
+            n = n>>1;
+        }
+        return count == 1;
+        
+    }
+    
+
+}
