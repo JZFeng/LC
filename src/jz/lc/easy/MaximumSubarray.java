1878a21ed1b95fde76904b297b25bdcf63b35a3a
JZFeng <matthew.fjz@gmail.com>
2017-06-02 19:14:20 -0700

    DP solution, keep result of max and min because of the nature of multiple two negative nubmer multiple  results in a positive number.

--- src/jz/lc/easy/MaximumSubarray.java
+++ src/jz/lc/easy/MaximumSubarray.java
@@ -37,28 +37,19 @@ public class MaximumSubarray
 
     public static int maxSubArray(int[] nums)
     {
-        int theSum = nums[0];
-        int maxSum = nums[0];
-        int start = 0;
-        int end = 0;
-        for(int i = 1; i < nums.length; i++)
-        {
-            theSum = theSum + nums[i];
-            if(theSum < nums[i])
-            {
-                theSum = nums[i];
-                start = i;
-            }
-            if(maxSum < theSum)
-            {
-                maxSum = theSum;
-                end = i;
-                
-            }
-        }
+        if(nums.length == 0) 
+            return 0;
+        if(nums.length == 1)
+            return nums[0];
         
-        System.out.println("start: " + nums[start] + " ; end: " + nums[end]);
-        return maxSum;
+        int global_sum = nums[0];
+        int sum = nums[0];
+        for(int i = 1; i < nums.length;i++){
+            sum = Math.max(sum+nums[i], nums[i]);
+            global_sum = Math.max(global_sum, sum);
+        }
+        return global_sum;
+            
     }
 
 }
