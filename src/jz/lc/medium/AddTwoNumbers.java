f355a957f7e71caaa5737edfc13f4cfcb1034c24
JZFeng <matthew.fjz@gmail.com>
2017-06-03 09:57:31 -0700

    Resolved

--- src/jz/lc/medium/AddTwoNumbers.java
+++ src/jz/lc/medium/AddTwoNumbers.java
@@ -17,71 +17,90 @@ import jz.LinkedList.ListNode;
 
 public class AddTwoNumbers
 {
-
-    public static void main(String[] args)
-    {
-        ListNode l1 = ListNode.listfromArray(new int[]{ 2, 4, 3 });
-        ListNode l2 = ListNode.listfromArray(new int[]{ 5, 6, 4 });
-        ListNode.printList(addTwoNumbers(l1, l2));
-
-    }
-
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2)
     {
-        int size1 = ListNode.size(l1);
-        int size2 = ListNode.size(l2);
-        ListNode longer = (size1 >= size2) ? l1 : l2;
-        ListNode res = longer;
-        ListNode shorter = (longer == l1) ? l2 : l1;
-        boolean isUp = false;
+        if (l1 == null || l2 == null)
+            return l1 == null ? l2 : l1;
 
-        while (shorter != null)
+        ListNode dummy1 = new ListNode(0);
+        dummy1.next = l1;
+        ListNode pre1 = dummy1;
+        ListNode cur1 = l1;
+
+        ListNode dummy2 = new ListNode(0);
+        dummy2.next = l2;
+        ListNode pre2 = dummy2;
+        ListNode cur2 = l2;
+
+        boolean hasCarry = false;
+
+        // store result into l2
+        while (cur1 != null && cur2 != null)
         {
-            longer.val = shorter.val + longer.val;
-            if (isUp)
+            if (hasCarry)
             {
-                longer.val++;
+                cur2.val++;
+                hasCarry = false;
             }
-
-            if (longer.val >= 10)
+            if ((cur1.val + cur2.val) >= 10)
             {
-                isUp = true;
-                longer.val = longer.val % 10;
-            }
-            shorter = shorter.next;
-            longer = longer.next;
-        }
-
-        while (longer != null && longer.next != null)
-        {
-            if (isUp)
-                longer.val++;
-
-            if (longer.val >= 10)
-            {
-                longer.val = longer.val % 10;
-                isUp = true;
-                longer = longer.next;
+                cur2.val = (cur1.val + cur2.val) % 10;
+                hasCarry = true;
             }
             else
-            {
-                return res;
-            }
+                cur2.val = cur1.val + cur2.val;
+
+            pre1 = cur1;
+            cur1 = cur1.next;
+            pre2 = cur2;
+            cur2 = cur2.next;
         }
 
-        if (longer != null)
+        if (cur2 == null && cur1 == null)
         {
-            if (isUp)
-                longer.val++;
-            if (longer.val >= 10)
+            if (hasCarry)
+                pre2.next = new ListNode(1);
+        }
+        else if (cur2 != null && hasCarry)
+        {
+            while (cur2 != null)
             {
-                longer.val = longer.val % 10;
-                longer.next = new ListNode(1);
+                if(++cur2.val <10)
+                    return dummy2.next;
+                else{
+                    cur2.val = cur2.val%10;
+                    hasCarry = true;
+                    pre2 = cur2;
+                    cur2 = cur2.next;
+                }
             }
-
+            if (hasCarry)
+                pre2.next = new ListNode(1);
+        }
+        else if(cur2!=null && !hasCarry)
+            return dummy2.next;
+        else    
+        {
+            pre2.next = cur1;
+            if(hasCarry)
+            {
+                while(cur1 != null){
+                    if(++cur1.val <10)
+                        return dummy2.next;
+                    else{
+                        cur1.val = cur1.val%10;
+                        hasCarry = true;
+                        pre1 = cur1;
+                        cur1 = cur1.next;
+                    }
+                }
+                if (hasCarry)
+                    pre1.next = new ListNode(1);
+            }
         }
 
-        return res;
+        return dummy2.next;
+
     }
 
 }
