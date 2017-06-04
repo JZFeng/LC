dbb4c65b5d241e0c498ca3bf3f2bf1a9a4e5f07a
JZFeng <matthew.fjz@gmail.com>
2017-06-04 15:54:00 -0700

    resolved

---
+++ src/jz/lc/medium/ProductofArrayExceptSelf.java
@@ -0,0 +1,51 @@
+/*
+ https://leetcode.com/problems/product-of-array-except-self/#/description
+ 
+ 238. Product of Array Except Self
+ 
+ Given an array of n integers where n > 1, nums, 
+ return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+
+Solve it without division and in O(n).
+
+For example, given [1,2,3,4], return [24,12,8,6].
+
+Follow up:
+Could you solve it with constant space complexity? (Note: The output array does not count as extra space for the purpose of space complexity analysis.)
+ 
+ */
+
+package jz.lc.medium;
+
+import jz.Util;
+
+public class ProductofArrayExceptSelf
+{
+
+    public static void main(String[] args)
+    {
+        Util.printArray(productExceptSelf(new int[]{2,3,4,5}));
+
+    }
+    
+    
+    public static int[] productExceptSelf(int[] nums) {
+    	int[] res = new int[nums.length];
+    	res[0] = 1;
+    	int left = 1;
+    	for(int i = 1; i < nums.length; i++){
+    		left *= nums[i-1];
+    		res[i] = left;
+    	}
+    	
+    	int right = 1;
+    	for(int i = nums.length-2; i>=0; i--){
+    		right *= nums[i+1];
+    		res[i] = res[i]*right;
+    	}
+
+    	return res;
+        
+    }
+
+}
