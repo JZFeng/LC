4f5b3857d89b9265e3daee2dd18e4b0ec76733cd
JZFeng <matthew.fjz@gmail.com>
2017-06-04 14:59:49 -0700

    use queue and two index

--- src/jz/lc/medium/BinaryTreeLevelOrderTraversal.java
+++ src/jz/lc/medium/BinaryTreeLevelOrderTraversal.java
@@ -23,7 +23,13 @@ return its level order traversal as:
  */
 package jz.lc.medium;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+
+import jz.BinarySearchTree.BinarySearchTree;
 import jz.BinarySearchTree.TreeNode;
 
 /**
@@ -35,12 +41,60 @@ public class BinaryTreeLevelOrderTraversal
 
     public static void main(String[] args)
     {
+        TreeNode root = TreeNode.fromString("1,2,3,4,5,6,7");
+        
+        List<List<Integer>> res = levelOrder(root);
+        for(List<Integer> a : res){
+            Iterator<Integer> itr = a.iterator();
+            while(itr.hasNext()){
+                System.out.print(itr.next() + " ");
+            }
+            System.out.println();
+        }
         
     }
     
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        return null;
+
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
         
+        if(root == null)
+            return res;
+        
+        TreeNode cur = root;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        List<Integer> curLevel = new ArrayList<Integer>();
+        List<Integer> nextLevel = new ArrayList<Integer>();
+        int numCur = 1;
+        int numNext = 0;
+        queue.offer(cur);
+        while(!queue.isEmpty()){
+            TreeNode tmp = queue.poll();
+            curLevel.add(tmp.val);
+            numCur--;
+            
+            if(tmp.left != null){
+                queue.offer(tmp.left);
+                numNext++;
+            }
+            if(tmp.right != null){
+                queue.offer(tmp.right);
+                numNext++;
+            }
+            
+            if(numCur == 0){
+                res.add(curLevel);
+                numCur = numNext;
+                numNext = 0;
+                curLevel = nextLevel;
+                nextLevel = new ArrayList<Integer>();
+            }
+            
+        }
+        
+        return res;
+    
     }
 
 }
