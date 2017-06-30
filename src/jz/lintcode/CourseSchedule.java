da3711dcf9b6cf3468c07e32b35620756f9442f8
JZFeng <matthew.fjz@gmail.com>
2017-06-30 00:12:51 -0700

    Topological sorting. [Number of Nodes and edges] representing a graph.

--- src/jz/lintcode/CourseSchedule.java
+++ src/jz/lintcode/CourseSchedule.java
@@ -9,49 +9,57 @@ import java.util.Queue;
 import java.util.Set;
 
 public class CourseSchedule {
-    /**
-     * @param numCourses a total of n courses
-     * @param prerequisites a list of prerequisite pairs
-     * @return true if can finish all courses or false
-     */
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        // Write your code here
-        List[] courses = new ArrayList[numCourses];
-        int[] indegree = new int[numCourses];
-        
-        // 建立领接表，存的是每个课程的前置课程 
-        //这里情况特殊，大多数我们存的是邻居。
-        for (int i = 0;i < numCourses; i++)
-            courses[i] = new ArrayList<Integer>();
-            
-        for (int i = 0; i < prerequisites.length; i++) {
-            indegree[prerequisites[i][0]] ++ ; //入度加1
-            courses[prerequisites[i][1]].add(prerequisites[i][0]); //加入前置课程
-        }
 
-        //入度为0的课程加入队列
-        Queue queue = new LinkedList();
-        for(int i = 0; i < indegree.length; i++){
-            if (indegree[i] == 0) {
-                queue.add(i);
-            }
-        }
-        
-        int count = 0;
-        while(!queue.isEmpty()){
-            int course = (int)queue.poll();
-            count ++;
-            //更新
-            int n = courses[course].size();
-            for(int i = 0; i < n; i++){
-                int pointer = (int)courses[course].get(i);
-                indegree[pointer]--;
-                if (indegree[pointer] == 0) {
-                    queue.add(pointer);
-                }
-            }
-        }
-        
-        return count == numCourses;
-    }
+	/**
+	 * @param numCourses
+	 *            a total of n courses
+	 * @param prerequisites
+	 *            a list of prerequisite pairs
+	 * @return true if can finish all courses or false
+	 */
+	public boolean canFinish(int numCourses, int[][] prerequisites) {
+
+		if (numCourses == 0) {
+			return true;
+		}
+		if (prerequisites == null || prerequisites.length == 0) {
+			return true;
+		}
+
+		// build up adjacency matrix and initianlize it
+		List[] courses = new ArrayList[numCourses];
+		for (int i = 0; i < courses.length; i++) {
+			courses[i] = new ArrayList<Integer>();
+		}
+
+		int[] indegree = new int[numCourses];
+		for (int i = 0; i < prerequisites.length; i++) {
+			indegree[prerequisites[i][0]]++;
+			courses[prerequisites[i][1]].add(prerequisites[i][0]);
+		}
+
+		Queue<Integer> queue = new LinkedList<Integer>();
+		for (int i = 0; i < indegree.length; i++) {
+			if (indegree[i] == 0) {
+				queue.offer(i);
+			}
+		}
+
+		int count = 0;
+		while (!queue.isEmpty()) {
+			int course = queue.poll();
+			count++;
+			ArrayList<Integer> neighbors = (ArrayList<Integer>) courses[course];
+			for (int i = 0; i < neighbors.size(); i++) {
+				indegree[neighbors.get(i)]--;
+				if (indegree[neighbors.get(i)] == 0) {
+					queue.offer(neighbors.get(i));
+				}
+			}
+		}
+
+		return count == numCourses;
+
+	}
+
 }
