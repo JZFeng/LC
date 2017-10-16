9babd272d8829b658c819f9ba552c7f393666c62
JZFeng <matthew.fjz@gmail.com>
2017-10-15 22:26:55 -0700

    split("\\.")

--- src/jz/lc/CompareVersionNumbers.java
+++ src/jz/lc/CompareVersionNumbers.java
@@ -8,10 +8,9 @@ public class CompareVersionNumbers {
 
     // 1.1 < 2.0
     public int compareVersion(String v1, String v2) {
-
-	String[] a1 = v1.split("\\.");
+	String[] a1 = v1.split("\\."); //attention !! split("\\.") NOT split(".");
 	String[] a2 = v2.split("\\.");
-	int maxLength = Math.max(a1.length, a2.length);
+	int maxLength = Math.max(a1.length, a2.length); //attention !! a1.length NOT v1.length();
 
 	for (int i = 0; i < maxLength; i++) {
 	    int num1 = i < a1.length ? Integer.parseInt(a1[i]) : 0;
