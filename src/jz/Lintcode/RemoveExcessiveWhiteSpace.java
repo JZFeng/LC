46809d0d1ab0b84a95a3d1ee871c29346e564440
jason <jason@192.168.31.97>
2017-11-12 07:44:45 -0800

    双指针，重写

--- src/jz/Lintcode/RemoveExcessiveWhiteSpace.java
+++ src/jz/Lintcode/RemoveExcessiveWhiteSpace.java
@@ -3,7 +3,10 @@ package jz.Lintcode;
 public class RemoveExcessiveWhiteSpace {
 
 	public static void main(String[] args) {
-
+		String s = "abc  def   jhk   abcc";
+		System.out.println(removeExcessiveWhiteSpace(s));
+		System.out.println(removeExcessiveWhiteSpaceII(s));
+		
 	}
 	
 	public static String removeExcessiveWhiteSpace(String s) {
@@ -19,6 +22,29 @@ public class RemoveExcessiveWhiteSpace {
 		sb.append(s.charAt(i));
 		return sb.toString().trim();
 	}
-
+	
+	public static String removeExcessiveWhiteSpaceII(String s) {
+		StringBuilder sb = new StringBuilder();
+		int i = 0 ;
+		int j = 0 ;
+		s = s.trim();
+		while(j < s.length()) {
+			while(j < s.length() && s.charAt(j) != ' ') {
+				j++;
+			}
+			while(i < s.length() && s.charAt(i) == ' ' ) {
+				i++;
+			}
+			if( i < j) {
+				sb.append(s.substring(i, j));
+				sb.append(" ");
+				i = j;
+				j = j + 1;
+			} else {
+				j++;
+			}
+		}
+		return sb.toString();
+	}
 
 }
