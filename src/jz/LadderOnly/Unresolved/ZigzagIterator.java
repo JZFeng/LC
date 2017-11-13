d749b730ad51aaa90d8f3d7a4a56df328bf08a86
jason <jason@192.168.31.97>
2017-11-12 20:45:22 -0800

    Done

--- src/jz/LadderOnly/Unresolved/ZigzagIterator.java
+++ src/jz/LadderOnly/Unresolved/ZigzagIterator.java
@@ -1,5 +1,54 @@
 package jz.LadderOnly.Unresolved;
-//http://lintcode.com/en/problem/zigzag-iterator/
-public class ZigzagIterator {
 
+import java.util.Iterator;
+import java.util.List;
+
+//http://lintcode.com/en/problem/zigzag-iterator/
+
+public class ZigzagIterator {
+	private List<Integer> v1;
+	private List<Integer> v2;
+	private Iterator<Integer> i1;
+	private Iterator<Integer> i2;
+	private boolean v1turn;
+
+	public ZigzagIterator(List<Integer> v1, List<Integer> v2) {
+		this.v1 = v1;
+		this.v2 = v2;
+		i1 = v1.iterator();
+		i2 = v2.iterator();
+		v1turn = true;
+	}
+
+	/*
+	 * @return: An integer
+	 */
+	public int next() {
+		int res = Integer.MAX_VALUE;
+		if (i1.hasNext() && i2.hasNext()) {
+			if (v1turn) {
+				res = i1.next();
+			} else {
+				res = i2.next();
+			}
+			v1turn = !v1turn;
+		} else if (i1.hasNext()) {
+			res = i1.next();
+			v1turn = true;
+		} else {
+			res = i2.next();
+			v1turn = false;
+		}
+
+		return res;
+	}
+
+	/*
+	 * @return: True if has next
+	 */
+	public boolean hasNext() {
+		// write your code here
+		return i1.hasNext() || i2.hasNext();
+	}
 }
+
