40ae9cd8b260d2616f6aca5451820c9cef212d5b
jason <jason@10.222.225.54>
2017-11-20 14:19:22 -0800

    reformat

--- src/jz/tree/trie/arrayImplment/Trie.java
+++ src/jz/tree/trie/arrayImplment/Trie.java
@@ -1,91 +1,64 @@
 package jz.tree.trie.arrayImplment;
 
 class TrieNode {
-    // Initialize your data structure here.
-    char value;
-    TrieNode[] children = new TrieNode[26];
-    boolean isLeaf;
+	// Initialize your data structure here.
+	char value;
+	TrieNode[] children = new TrieNode[26];
+	boolean isLeaf;
 
-    public TrieNode(char c) {
-	this.value = c;
-	this.isLeaf = false;
-    }
+	public TrieNode(char c) {
+		this.value = c;
+		this.isLeaf = false;
+	}
 }
 
 public class Trie {
-    private TrieNode root;
+	private TrieNode root;
 
-    public static void main(String[] args) {
-	Trie trie = new Trie();
-	trie.insert("lintcode");
-	trie.insert("word");
-	System.out.println(trie.search("Word"));
-	System.out.println(trie.startsWith("lint"));
-    }
-    
-    public Trie() {
-	// do intialization if necessary
-	root = new TrieNode(' ');
-    }
-
-    /*
-     * @param word: a word
-     * 
-     * @return: nothing
-     */
-    public void insert(String word) {
-	// write your code here
-	char[] data = word.toLowerCase().toCharArray();
-	TrieNode cur = root;
-	for (int i = 0; i < data.length; i++) {
-	    int index = data[i] - 'a';
-	    if (cur.children[index] == null) {
-		cur.children[index] = new TrieNode(data[i]);
-	    }
-	    cur = cur.children[index];
-	}
-	cur.isLeaf = true;
-    }
-
-    /*
-     * @param word: A string
-     * 
-     * @return: if the word is in the trie.
-     */
-    public boolean search(String word) {
-	// write your code here
-	char[] data = word.toLowerCase().toCharArray();
-	TrieNode cur = root;
-	for (int i = 0; i < data.length; i++) {
-	    int index = data[i] - 'a';
-	    if (cur.children[index] == null) {
-		return false;
-	    }
-	    cur = cur.children[index];
+	public Trie() {
+		root = new TrieNode(' ');
 	}
 
-	return cur.isLeaf;
-    }
+	public void insert(String word) {
 
-    /*
-     * @param prefix: A string
-     * 
-     * @return: if there is any word in the trie that starts with the given
-     * prefix.
-     */
-    public boolean startsWith(String prefix) {
-	// write your code here
-	char[] data = prefix.toLowerCase().toCharArray();
-	TrieNode cur = root;
-	for (int i = 0; i < data.length; i++) {
-	    int index = data[i] - 'a';
-	    if (cur.children[index] == null) {
-		return false;
-	    }
-	    cur = cur.children[index];
+		char[] data = word.toLowerCase().toCharArray();
+		TrieNode cur = root;
+		for (int i = 0; i < data.length; i++) {
+			int index = data[i] - 'a';
+			if (cur.children[index] == null) {
+				cur.children[index] = new TrieNode(data[i]);
+			}
+			cur = cur.children[index];
+		}
+		cur.isLeaf = true;
 	}
 
-	return true;
+	public boolean search(String word) {
+		char[] data = word.toLowerCase().toCharArray();
+		TrieNode cur = root;
+		for (int i = 0; i < data.length; i++) {
+			int index = data[i] - 'a';
+			if (cur.children[index] == null) {
+				return false;
+			}
+			cur = cur.children[index];
+		}
 
-    }
+		return cur.isLeaf;
+	}
+
+	public boolean startsWith(String prefix) {
+		char[] data = prefix.toLowerCase().toCharArray();
+		TrieNode cur = root;
+		for (int i = 0; i < data.length; i++) {
+			int index = data[i] - 'a';
+			if (cur.children[index] == null) {
+				return false;
+			}
+			cur = cur.children[index];
+		}
+
+		return true;
+
+	}
 }
--- src/jz/tree/trie/hashmapImplement/Trie.java
+++ src/jz/tree/trie/hashmapImplement/Trie.java
@@ -3,78 +3,70 @@ package jz.tree.trie.hashmapImplement;
 import java.util.HashMap;
 
 class TrieNode {
-    char val;
-    HashMap<Character, TrieNode> children = null;
-    boolean isLeaf;
+	char val;
+	HashMap<Character, TrieNode> children = null;
+	boolean isLeaf;
 
-    TrieNode(char val) {
-	this.val = val;
-	this.children = new HashMap<Character, TrieNode>();
-	this.isLeaf = false;
-    }
+	TrieNode(char val) {
+		this.val = val;
+		this.children = new HashMap<Character, TrieNode>();
+		this.isLeaf = false;
+	}
 }
 
 public class Trie {
-    private TrieNode root;
+	private TrieNode root;
 
-    public static void main(String[] args) {
-	Trie trie = new Trie();
-	trie.insert("lintcode");;
-	trie.insert("word");;
-	System.out.println(trie.search("word"));
-	System.out.println(trie.startsWith("lint"));
-    }
-    
-    Trie() {
-	this.root = new TrieNode(' ');
-    }
-
-    public void insert(String word) {
-	char[] data = word.toLowerCase().toCharArray();
-	TrieNode cur = root;
-	HashMap<Character, TrieNode> children = cur.children;
-	for (int i = 0; i < data.length; i++) {
-	    char c = data[i];
-	    if (!children.containsKey(c)) {
-		TrieNode node = new TrieNode(c);
-		children.put(c, node);
-	    }
-	    cur = children.get(c);
-	    children = cur.children;
-	}
-	cur.isLeaf = true;
-    }
-
-    public boolean search(String word) {
-	TrieNode cur = root;
-	HashMap<Character, TrieNode> children = cur.children;
-
-	char[] data = word.toLowerCase().toCharArray();
-	for (int i = 0; i < data.length; i++) {
-	    char c = data[i];
-	    if (!children.containsKey(c)) {
-		return false;
-	    }
-	    cur = children.get(c);
-	    children = cur.children;
+	Trie() {
+		this.root = new TrieNode(' ');
 	}
 
-	return cur.isLeaf;
-    }
-
-    public boolean startsWith(String prefix) {
-	TrieNode cur = root;
-	HashMap<Character, TrieNode> children = cur.children;
-	char[] data = prefix.toLowerCase().toCharArray();
-	for (int i = 0; i < data.length; i++) {
-	    char c = data[i];
-	    if (!children.containsKey(c)) {
-		return false;
-	    }
-	    cur = children.get(c);
-	    children = cur.children;
+	public void insert(String word) {
+		char[] data = word.toLowerCase().toCharArray();
+		TrieNode cur = root;
+		HashMap<Character, TrieNode> children = cur.children;
+		for (int i = 0; i < data.length; i++) {
+			char c = data[i];
+			if (!children.containsKey(c)) {
+				TrieNode node = new TrieNode(c);
+				children.put(c, node);
+			}
+			cur = children.get(c);
+			children = cur.children;
+		}
+		cur.isLeaf = true;
 	}
 
-	return true;
-    }
+	public boolean search(String word) {
+		TrieNode cur = root;
+		HashMap<Character, TrieNode> children = cur.children;
+
+		char[] data = word.toLowerCase().toCharArray();
+		for (int i = 0; i < data.length; i++) {
+			char c = data[i];
+			if (!children.containsKey(c)) {
+				return false;
+			}
+			cur = children.get(c);
+			children = cur.children;
+		}
+
+		return cur.isLeaf;
+	}
+
+	public boolean startsWith(String prefix) {
+		TrieNode cur = root;
+		HashMap<Character, TrieNode> children = cur.children;
+		char[] data = prefix.toLowerCase().toCharArray();
+		for (int i = 0; i < data.length; i++) {
+			char c = data[i];
+			if (!children.containsKey(c)) {
+				return false;
+			}
+			cur = children.get(c);
+			children = cur.children;
+		}
+
+		return true;
+	}
 }
