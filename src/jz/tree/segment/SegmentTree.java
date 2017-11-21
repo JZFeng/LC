012e96da9717c4ed46b454c801935a4ae14477fa
jason <jason@10.222.225.54>
2017-11-20 16:42:39 -0800

    reformat

--- src/jz/tree/segment/SegmentTree.java
+++ src/jz/tree/segment/SegmentTree.java
@@ -1,47 +1,48 @@
 package jz.tree.segment;
 
 class SegmentTreeNode {
-    int start;
-    int end;
-    SegmentTreeNode left;
-    SegmentTreeNode right;
-    
-    SegmentTreeNode(int start, int end) {
-	this.start = start;
-	this.end = end;
-	this.left = null;
-	this.right = null;
-    }
+	int start;
+	int end;
+	SegmentTreeNode left;
+	SegmentTreeNode right;
+
+	SegmentTreeNode(int start, int end) {
+		this.start = start;
+		this.end = end;
+		this.left = null;
+		this.right = null;
+	}
 }
 
 public class SegmentTree {
-    SegmentTreeNode root ;
-    
-    public static void main(String[] args) {
-	SegmentTree st = new SegmentTree(1,10);
-	System.out.println(st);
-    }
-    
-    SegmentTree() {
-	root = null;
-    }
-    
-    SegmentTree(int start, int end) {
-	this.root = build(start, end);
-    }
-    public SegmentTreeNode build(int start, int end) {
-	if(start > end) {
-	    return null;
+	SegmentTreeNode root;
+
+	public static void main(String[] args) {
+		SegmentTree st = new SegmentTree(1, 10);
+		System.out.println(st);
 	}
-	
-	SegmentTreeNode root = new SegmentTreeNode(start, end);
-	
-	if(start != end) {
-	    int mid = (start + end ) / 2;
-	    root.left = build(start, mid);
-	    root.right = build(mid + 1, end);
+
+	SegmentTree() {
+		root = null;
 	}
-	
-	return root;
-    }
+
+	SegmentTree(int start, int end) {
+		this.root = build(start, end);
+	}
+
+	public SegmentTreeNode build(int start, int end) {
+		if (start > end) {
+			return null;
+		}
+
+		SegmentTreeNode root = new SegmentTreeNode(start, end);
+
+		if (start != end) {
+			int mid = (start + end) / 2;
+			root.left = build(start, mid);
+			root.right = build(mid + 1, end);
+		}
+
+		return root;
+	}
 }
